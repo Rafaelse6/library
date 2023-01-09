3f82a96b8e1329bafe7c1505030bf418c3f62454
Rafael Santos <rafa.05125@gmail.com>
2023-01-09 19:19:35 -0300

    CategoryService

---
+++ backend/src/main/java/com/rafaelsantos/library/services/CategoryService.java
@@ -0,0 +1,22 @@
+package com.rafaelsantos.library.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rafaelsantos.library.entities.Category;
+import com.rafaelsantos.library.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	@Transactional(readOnly = true)
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}

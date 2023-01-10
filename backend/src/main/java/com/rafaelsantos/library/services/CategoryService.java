471029f4435243e5f62cb9c0ff0499073c0b231d
Rafael Santos <rafa.05125@gmail.com>
2023-01-10 19:39:57 -0300

    Category CRUD

--- backend/src/main/java/com/rafaelsantos/library/services/CategoryService.java
+++ backend/src/main/java/com/rafaelsantos/library/services/CategoryService.java
@@ -1,13 +1,20 @@
 package com.rafaelsantos.library.services;
 
-import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.rafaelsantos.library.entities.Category;
+import com.rafaelsantos.library.entities.dto.CategoryDTO;
 import com.rafaelsantos.library.repositories.CategoryRepository;
+import com.rafaelsantos.library.services.exceptions.DatabaseException;
+import com.rafaelsantos.library.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
@@ -16,7 +23,49 @@ public class CategoryService {
 	private CategoryRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public Page<CategoryDTO> findAllPaged(Pageable pageable){
+		Page<Category> list = repository.findAll(pageable);
+		return list.map(x -> new CategoryDTO(x));
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new CategoryDTO(entity);
+	}
+	
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = repository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
+	
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			repository.save(entity);
+			
+			return new CategoryDTO(entity);
+		} catch (Exception e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }

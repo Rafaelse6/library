05f54d66207092039b027858fe408f589853c747
Rafael Santos <rafa.05125@gmail.com>
2023-01-13 16:45:29 -0300

    Authorizations confirmed

--- backend/src/main/java/com/rafaelsantos/library/config/ResourceServerConfig.java
+++ backend/src/main/java/com/rafaelsantos/library/config/ResourceServerConfig.java
@@ -18,25 +18,25 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	
 	@Autowired
 	private Environment env;
-
+	
 	@Autowired
 	private JwtTokenStore tokenStore;
-	
-	private static final String [] PUBLIC = {"/oauth/token/**", "/h2-console/**"};
-	
+
+	private static final String [] PUBLIC = {"/oauth/token", "/h2-console/**"};
+
 	private static final String [] CLIENT_OR_ADMIN = {"/books/**", "/categories/**"};
-	
+
 	private static final String [] ADMIN = {"/users/**"};
-	
+
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
 	}
 
+
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		
-		//H2
 		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
@@ -44,9 +44,11 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
 		.antMatchers(HttpMethod.GET, CLIENT_OR_ADMIN).permitAll()
-		.antMatchers(CLIENT_OR_ADMIN).hasAnyRole("CLIENT", "ADMIN")
+		.antMatchers(HttpMethod.POST, CLIENT_OR_ADMIN).hasRole("ADMIN")
+		.antMatchers(HttpMethod.PUT, CLIENT_OR_ADMIN).hasRole("ADMIN")
+		.antMatchers(HttpMethod.DELETE, CLIENT_OR_ADMIN).hasRole("ADMIN")
 		.antMatchers(ADMIN).hasRole("ADMIN")
 		.anyRequest().authenticated();
 	}
-	
+
 }
